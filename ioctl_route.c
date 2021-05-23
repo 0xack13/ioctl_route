c27c7a2b2c76f28384bf8d8550b88d5d6104a544
0xack13 <0xack13@gmail.com>
2021-05-23 19:31:39 +1000

    Add route using ioctl

--- ioctl_route.c
+++ ioctl_route.c
@@ -1,5 +1,38 @@
 #include<stdio.h>
+#include<stdlib.h>
+#include<sys/socket.h>
+#include<netinet/in.h>
+#include<arpa/inet.h>
 int main()
 {
-	printf("hello -- 2");	
+    // socket file descriptor
+    int sockfd, new_socket, c;
+    struct sockaddr_in name, client;
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0)
+    {
+        perror("socket");
+        exit(EXIT_FAILURE);
+    }
+	bzero(&name, sizeof(name));
+    name.sin_family = AF_INET;
+    name.sin_port = htons(8080);
+    name.sin_addr.s_addr = INADDR_ANY;
+    if (bind(sockfd, (struct sockaddr *) &name, sizeof(name)) < 0)
+    {
+        perror("bind");
+        exit(EXIT_FAILURE);
+    }
+    listen(sockfd, 3);
+    printf("socket created!");
+    //Accept and incoming connection
+	puts("Waiting for incoming connections...");
+	c = sizeof(struct sockaddr_in);
+	new_socket = accept(sockfd, (struct sockaddr *)&client, (socklen_t*)&c);
+	if (new_socket<0)
+	{
+		perror("accept failed");
+	}
+
+	puts("Connection accepted");
 }
